86a16e81350056eba4f00ff8d29a21200fb61d6c
Victoria Meng <nights1ststar@protonmail.com>
2018-07-03 20:52:37 +0000

    automatically backed up by learn

--- app/models/my_validator.rb
+++ app/models/my_validator.rb
@@ -1,6 +1,5 @@
 class MyValidator < ActiveModel::Validator
   def validate(post)
-    # binding.pry
     clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
     if post.title && clickbait.none? { |phrase| post.title.include?(phrase) }
       post.errors[:title] << "Need clickbait"
